88bb56a8ce74ab74c617686eac24d069af9830f8
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-06-07 16:50:31 +0300

    add all game code

---
+++ script.js
@@ -0,0 +1,86 @@
+let countShout = 0;
+let currentPlayer = 1;
+let rotateBaraban = 0;
+let bulletPosition = getRandomBullet(1, 6);
+const btnShot = document.querySelector('#shot');
+const revolver = document.querySelector('#revolver');
+const bullet = document.querySelector('#bullet');
+const baraban = document.querySelector('#baraban');
+
+function startGame() {
+  btnShot.className = 'off';
+  bullet.style.display = 'block';
+  revolver.style.display = 'block';
+  btnShot.onclick = null;
+  let rotateDegree = 0;
+
+  const timer = setInterval(function () {
+    rotateDegree += 10;
+    baraban.style.transform = `rotate(${rotateDegree}deg)`;
+
+    if (rotateDegree > 300) {
+      bullet.style.display = 'none';
+    }
+    if (rotateDegree == 720) {
+      clearInterval(timer);
+      btnShot.innerText = 'Make a shot';
+      btnShot.onclick = makeShot;
+      btnShot.className = '';
+    }
+  }, 50);
+}
+
+function rotationRight() {
+  revolver.style.background = 'url("images/revolver-right.png") no-repeat';
+}
+function rotationLeft() {
+  revolver.style.background = 'url("images/revolver-left.png") no-repeat';
+}
+
+function endGame() {
+  alert('Game over!');
+  btnShot.onclick = restart;
+  btnShot.innerText = 'Restart';
+}
+
+function getRandomBullet(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+btnShot.onclick = startGame;
+
+function makeShot() {
+  countShout += 1;
+  if (countShout == bulletPosition) {
+    showBlood();
+    endGame();
+  } else {
+    if (currentPlayer == 1) {
+      rotationRight();
+      currentPlayer = 2;
+    } else {
+      rotationLeft();
+      currentPlayer = 1;
+    }
+    let rotate = rotateBaraban;
+    const barabanTimer = setInterval(function () {
+      rotate += 10;
+      baraban.style.transform = `rotate(${rotate}deg)`;
+      if ((rotate = rotateBaraban + 60)) {
+        clearInterval(barabanTimer);
+        rotateBaraban = rotate;
+      }
+    }, 10);
+  }
+}
+
+function restart() {
+  location.reload();
+}
+
+function showBlood() {
+  let bloodElement = document.createElement('div');
+  bloodElement.id = 'blood';
+  let playerElement = document.querySelector('#player' + currentPlayer);
+  playerElement.appendChild(bloodElement);
+}
